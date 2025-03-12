3668f188ab6234ea27b02785d0b9d3c6b5068847
PierrunoYT <pierrebruno@hotmail.ch>
2025-03-12 12:53:01 +0100

    Remove unused theme-related files

--- frontend/src/components/ThemeScript.tsx
+++
@@ -1,4 +0,0 @@
-// This component is no longer needed as theme initialization is handled in layout.tsx
-export default function ThemeScript() {
-  return null;
-}
--- frontend/src/utils/ThemeContext.tsx
+++
@@ -1,2 +0,0 @@
-// This file is deprecated - using providers.tsx instead
-export { ThemeProvider, useTheme } from '../app/providers';
